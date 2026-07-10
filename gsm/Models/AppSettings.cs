c994863c21d4eb2ec8167449b974f21b5f0cc78e
KongNguyeen <congnbn45@gmail.com>
2026-07-10 10:59:07 +0700

    test

--- gsm/Models/AppSettings.cs
+++ gsm/Models/AppSettings.cs
@@ -4,12 +4,12 @@ namespace gsm.Models;
 
 public class AppSettings
 {
-    public string TelegramBotToken { get; set; } = "8926115937:AAFpUEvxfFqRpwGDWChbEQEWsn6xkZ-RTCQ";
-    public string TelegramChatIds { get; set; } = "-1003586587027";
+    public string TelegramBotToken { get; set; } = "";
+    public string TelegramChatIds { get; set; } = "";
     public bool ReceiveAllSms { get; set; } = true;
     public bool EnableTelegramNotification { get; set; } = true;
     public bool EnableWebNotification { get; set; } = true;
-    public string FirebaseUrl { get; set; } = "https://toolweb-c7702-default-rtdb.firebaseio.com/";
+    public string FirebaseUrl { get; set; } = "";
     
     // Tự động chuyển hướng cuộc gọi
     public bool EnableAutoCallForwarding { get; set; } = false;
